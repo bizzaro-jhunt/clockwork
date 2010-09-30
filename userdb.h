cce88ad25f130578053361afc54f3b3e767e5c89
James Hunt <filefrog@gmail.com>
2010-09-30 00:04:09 -0500

    Remove comments about 'const' qualifiers

--- userdb.h
+++ userdb.h
@@ -34,17 +34,17 @@ struct sgdb {
 };
 
 struct pwdb* pwdb_init(const char *path);
-struct passwd* pwdb_get_by_name(struct pwdb *db, const char *name); /* FIXME: needs const qualifier */
-struct passwd* pwdb_get_by_uid(struct pwdb *db, uid_t uid); /* FIXME: needs const qualifier */
-int pwdb_add(struct pwdb *db, struct passwd *pw); /* FIXME: needs const qualifier */
-int pwdb_rm(struct pwdb *db, struct passwd *pw); /* FIXME: needs const qualifier */
+struct passwd* pwdb_get_by_name(struct pwdb *db, const char *name);
+struct passwd* pwdb_get_by_uid(struct pwdb *db, uid_t uid);
+int pwdb_add(struct pwdb *db, struct passwd *pw);
+int pwdb_rm(struct pwdb *db, struct passwd *pw);
 int pwdb_write(struct pwdb *db, const char *file);
 void pwdb_free(struct pwdb *db);
 
 struct spdb* spdb_init(const char *path);
-struct spwd* spdb_get_by_name(struct spdb *db, const char *name); /* FIXME: needs const qualifier */
-int spdb_add(struct spdb *db, struct spwd *sp); /* FIXME: needs const qualifier */
-int spdb_rm(struct spdb *db, struct spwd *sp); /* FIXME: needs const qualifier */
+struct spwd* spdb_get_by_name(struct spdb *db, const char *name);
+int spdb_add(struct spdb *db, struct spwd *sp);
+int spdb_rm(struct spdb *db, struct spwd *sp);
 int spdb_write(struct spdb *db, const char *file);
 void spdb_free(struct spdb *db);
 

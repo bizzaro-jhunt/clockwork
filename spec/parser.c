c3060cf0376aba375400a1c78cad76a6cae5136e
James Hunt <filefrog@gmail.com>
2010-11-07 22:20:28 -0600

    Comment and Code cleanups in parse_file

--- spec/parser.c
+++ spec/parser.c
@@ -8,15 +8,11 @@ struct ast* parse_file(const char *path)
 	spec_parser_context ctx;
 	struct ast *root;
 
-	/* FIXME: move to spec_parser_new() contructor */
 	ctx.file = NULL;
 	ctx.files = stringlist_new(NULL);
 	list_init(&ctx.fseen);
 
 	yylex_init_extra(&ctx, &ctx.scanner);
-	/*
-	lexer_init_file(path, &ctx);
-	*/
 	lexer_include_file(path, &ctx);
 	yyparse(&ctx);
 

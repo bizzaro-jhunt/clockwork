7b0eb8060fcb43beff8a74239cd3e428ad35b6da
James Hunt <james@niftylogic.com>
2013-03-02 23:03:21 -0500

    0.2.8-1 Build Tweaks
     - Fix the clockwork-server init script
     - Start clockwork-server post-install
     - Bundle the database schemas in -server and -agent
     - Fix paths for Debian (/var/lock, etc.)
     - Package help files (for cwpol) properly
     - Add maintainer scripts for db initialization

--- debian/init.d
+++ debian/init.d
@@ -1,23 +1,23 @@
 #!/bin/sh
 ### BEGIN INIT INFO
-# Provides:          clockwork
+# Provides:          clockwork-server
 # Required-Start:    $network $local_fs
 # Required-Stop:     $network $local_fs
 # Default-Start:     2 3 4 5
 # Default-Stop:      0 1 6
-# Short-Description: <Enter a short description of the sortware>
-# Description:       <Enter a long description of the software>
-#                    <...>
-#                    <...>
+# Short-Description: Secure Configuration Management Server
+# Description:       Clockwork is a safe, secure and flexible
+#                    system for managing system configuration
+#                    through enforcement of targeted policies.
 ### END INIT INFO
 
-# Author: James Hunt <jrhunt@unknown>
+# Author: James Hunt <james@niftylogic.com>
 
 PATH=/sbin:/bin
-DESC=clockwork             # Introduce a short description here
-NAME=clockwork             # Introduce the short server's name here
-DAEMON=/sbin/policyd       # Introduce the server's location here
-DAEMON_ARGS=""             # Arguments to run the daemon with
+DESC=clockwork
+NAME=policyd
+DAEMON=/sbin/policyd
+DAEMON_ARGS=""
 PIDFILE=/var/run/$NAME.pid
 SCRIPTNAME=/etc/init.d/policyd
 
@@ -34,11 +34,7 @@ SCRIPTNAME=/etc/init.d/policyd
 # Depend on lsb-base (>= 3.0-6) to ensure that this file is present.
 . /lib/lsb/init-functions
 
-#
-# Function that starts the daemon/service
-#
-do_start()
-{
+do_start() {
 	# Return
 	#   0 if daemon has been started
 	#   1 if daemon was already running
@@ -53,11 +49,7 @@ do_start()
 	# on this one.  As a last resort, sleep for some time.
 }
 
-#
-# Function that stops the daemon/service
-#
-do_stop()
-{
+do_stop() {
 	# Return
 	#   0 if daemon has been stopped
 	#   1 if daemon was already stopped
@@ -79,29 +71,21 @@ do_stop()
 	return "$RETVAL"
 }
 
-#
-# Function that sends a SIGHUP to the daemon/service
-#
 do_reload() {
-	#
-	# If the daemon can reload its configuration without
-	# restarting (for example, when it is sent a SIGHUP),
-	# then implement that here.
-	#
 	start-stop-daemon --stop --signal 1 --quiet --pidfile $PIDFILE --name $NAME
 	return 0
 }
 
 case "$1" in
-  start)
-    [ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC " "$NAME"
-    do_start
-    case "$?" in
+start)
+	[ "$VERBOSE" != no ] && log_daemon_msg "Starting $DESC " "$NAME"
+	do_start
+	case "$?" in
 		0|1) [ "$VERBOSE" != no ] && log_end_msg 0 ;;
 		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
 	esac
-  ;;
-  stop)
+	;;
+stop)
 	[ "$VERBOSE" != no ] && log_daemon_msg "Stopping $DESC" "$NAME"
 	do_stop
 	case "$?" in
@@ -109,43 +93,34 @@ case "$1" in
 		2) [ "$VERBOSE" != no ] && log_end_msg 1 ;;
 	esac
 	;;
-  status)
-       status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
-       ;;
-  #reload|force-reload)
-	#
-	# If do_reload() is not implemented then leave this commented out
-	# and leave 'force-reload' as an alias for 'restart'.
-	#
-	#log_daemon_msg "Reloading $DESC" "$NAME"
-	#do_reload
-	#log_end_msg $?
-	#;;
-  restart|force-reload)
-	#
-	# If the "reload" option is implemented then remove the
-	# 'force-reload' alias
-	#
+status)
+	status_of_proc "$DAEMON" "$NAME" && exit 0 || exit $?
+	;;
+reload|force-reload)
+	log_daemon_msg "Reloading $DESC" "$NAME"
+	do_reload
+	log_end_msg $?
+	;;
+restart)
 	log_daemon_msg "Restarting $DESC" "$NAME"
 	do_stop
 	case "$?" in
-	  0|1)
+	0|1)
 		do_start
 		case "$?" in
-			0) log_end_msg 0 ;;
-			1) log_end_msg 1 ;; # Old process is still running
-			*) log_end_msg 1 ;; # Failed to start
+		0) log_end_msg 0 ;;
+		1) log_end_msg 1 ;; # Old process is still running
+		*) log_end_msg 1 ;; # Failed to start
 		esac
 		;;
-	  *)
-	  	# Failed to stop
+	*)
+		# Failed to stop
 		log_end_msg 1
 		;;
 	esac
 	;;
-  *)
-	#echo "Usage: $SCRIPTNAME {start|stop|restart|reload|force-reload}" >&2
-	echo "Usage: $SCRIPTNAME {start|stop|status|restart|force-reload}" >&2
+*)
+	echo "Usage: $SCRIPTNAME {start|stop|status|restart|reload|force-reload}" >&2
 	exit 3
 	;;
 esac

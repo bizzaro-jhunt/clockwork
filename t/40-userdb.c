c44127183d6bc2add5873ab3063caf67538e7cca
James Hunt <james@niftylogic.com>
2013-09-21 10:22:12 -0400

    Finish conversion to TAP (+coverage)
    For DEBT-7

--- t/40-userdb.c
+++ t/40-userdb.c
@@ -57,8 +57,8 @@ static void assert_sgdb_get(struct sgdb *db, const char *name)
 	gr = sgdb_get_by_name(db, name);
 
 	snprintf(buf, 256, "Look up %s sgrp by name", name);
-	assert_not_null(buf, gr);
-	assert_str_eq(buf, gr->sg_namp, name);
+	isnt_null(buf, gr);
+	is_string(buf, gr->sg_namp, name);
 }
 
 /*********************************************************/

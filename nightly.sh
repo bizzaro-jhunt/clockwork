dcd58371f1cd9642481f295a48a6c452f56c386d
James Hunt <james@niftylogic.com>
2015-01-13 18:57:32 -0500

    Update LXC testing scripts

--- nightly.sh
+++ nightly.sh
@@ -16,11 +16,13 @@ cd ..
 rm -rf _nightly
 
 if [ "x$EXPORT" != "x" ]; then
+	[ $EXPORT -lt 1024 ] && EXPORT=9909
+	WHERE=$(ip addr show | grep inet.*global | sed -e 's/.*inet //;s@/.*@@;' | head -n1)
 	echo
 	echo "Ready."
 	echo
 	echo "To load into a nightly LXC tester,"
-	echo "run \`cd /; nc `hostname -f` $EXPORT | tar -xzv\`"
+	echo "run \`cd /; nc $WHERE $EXPORT | tar -xzv\`"
 	echo "from inside the container environment."
 	nc -l $EXPORT < nightly.tar.gz
 fi
